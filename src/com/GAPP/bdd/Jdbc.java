054dc5b2bed1cc14b514c6d422d810025c7efd53
AlexKD7 <alexis.ung@isep.fr>
2016-05-10 11:17:13 +0200

    Connection

--- src/com/GAPP/bdd/Jdbc.java
+++ src/com/GAPP/bdd/Jdbc.java
@@ -1,10 +1,25 @@
 package com.GAPP.bdd;
 
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+
 public class Jdbc {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//test
+		
+	}
+	public static void sauverEnBase(String personne){
+		// Information d'accès à la base de donnéees
+		String url = "jdbc:mysql://localhost/gapp";
+		String login = "root";
+		String passwd = "";
+		Connection cn =null;
+		Statement st =null;
+		try{
+			
+		}
+		
 	}
 
 }
