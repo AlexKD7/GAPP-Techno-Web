3156a5aa2d8779fd6655d29d3260e29d54f023b2
AlexKD7 <alexis.ung@isep.fr>
2016-05-24 11:16:56 +0200

    JDBC
    JDBC

--- src/com/GAPP/bdd/Jdbc.java
+++ src/com/GAPP/bdd/Jdbc.java
@@ -14,25 +14,32 @@ public class Jdbc {
 	}
 	public static void sauverEnBase(String login){
 		// Information d'accès à la base de donnéees
-		String url = "jdbc:mysql://localhost/gapp";
-		String login1 = "admin";
-		String passwd = "1234";
+		System.out.println("eee");
+		String URL = "jdbc:mysql://localhost/Gapp1";
+		System.out.println("eee1");
+		String login1 = "root";
+		String passwd = "root";
 		Connection cn =null;
 		Statement st =null;
 		try{
 			//Etape 1 : Chargement du driver
 			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("eee2");
 			//Etape 2 : Récupération de la connexion
-			cn = (Connection) DriverManager.getConnection(url, login1, passwd);
+			cn = (Connection) DriverManager.getConnection(URL, login1, passwd);
+			System.out.println("eee3");
 			//Etape 3 : Création d'un statement
 			st = (Statement) cn.createStatement();
-			String sql = "INSERT INTO 'utilisateur' ('login') VALUES ('"+ login +"')";
+			System.out.println("eee4");
+			String sql = "INSERT INTO 'utilisateur' ('login1') VALUES ('"+ login1 +"')";
 			//Etape 4 : execution requête
 			st.executeUpdate(sql);
-			
+			System.out.println("eee5");
 		   } catch (SQLException e){
+			   System.out.println("eee6");
 			   e.printStackTrace();
 		   } catch (ClassNotFoundException e){
+			   System.out.println("eee7");
 			   e.printStackTrace();
 		   } finally {
 			   try {
